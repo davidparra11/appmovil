a763cfe713670eaaf88394286c7d83732afa0aa6
davidparra <david.parra@utp.edu.co>
2016-05-08 02:15:19 -0500

    integratin

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,27 @@
-angular.module('starter.controllers', [])
+angular.module('directory.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('EmployeeListCtrl', function ($scope, Employees) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+        $scope.searchKey = "";
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+        $scope.clearSearch = function () {
+            $scope.searchKey = "";
+            $scope.employees = Employees.query();
+        }
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+        $scope.search = function () {
+            $scope.employees = Employees.query({name: $scope.searchKey});
+        }
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+        $scope.employees = Employees.query();
+    })
+
+    .controller('EmployeeDetailCtrl', function($scope, $stateParams, Employees) {
+        console.log('details');
+        $scope.employee = Employees.get({employeeId: $stateParams.employeeId});
+    })
+
+    .controller('EmployeeReportsCtrl', function ($scope, $stateParams, Employees) {
+        console.log('reports');
+        $scope.employee = Employees.get({employeeId: $stateParams.employeeId, data: 'reports'});
+    });
