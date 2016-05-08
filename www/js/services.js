016a9a3f738b58021d895adc297f8fa38e20b845
davidparra <david.parra@utp.edu.co>
2016-05-08 11:20:25 -0500

    Revert "delivery correct person (LGTM)"
    This reverts commit 5c25c64a44aef705f81cde0c3350953c2a85687b.

--- www/js/services.js
+++ www/js/services.js
@@ -6,24 +6,29 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Javier Beltran',
-    lastText: 'Gerente General',
-    face: 'img/javier.png'
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
   }, {
     id: 1,
-    name: 'Danilo Betancourt',
-    lastText: 'Gerente de Desarrollo',
-    face: 'img/danilo.png'
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
   }, {
     id: 2,
-    name: 'David Parra',
-    lastText: 'Desarrollador',
-    face: 'img/david.png'
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
   }, {
     id: 3,
-    name: 'Juan Camilo',
-    lastText: 'Desarrollador',
-    face: 'img/juan.png'
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
   }];
 
   return {
