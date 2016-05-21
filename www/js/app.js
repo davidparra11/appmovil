4af3150edf8b0f46aaad0288bc99f8da8e8d50d4
davidparra <david.parra@utp.edu.co>
2016-05-21 02:25:51 -0500

    making style changes and re-identing syntax

--- www/js/app.js
+++ www/js/app.js
@@ -6,76 +6,75 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-//, 'ngMockE2E'
+  //, 'ngMockE2E'
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-.config(function($stateProvider, $urlRouterProvider) {
- 
-  $stateProvider
-  .state('menu', {
-    url: '/menu',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'MenuCtrl'
-  })
-  .state('login', {
-      url: '/login',
-      templateUrl: 'templates/login.html',
-      controller: 'LoginCtrl'
-    })
-  .state('menu.citas', {
-    url: '/citas',
-      views:{
-        'contenido': {
-          templateUrl: 'templates/citas.html'
-        }
       }
-  })
-  .state('menu.digiturno', {
-    url: '/digiturno',
-      views:{
-        'contenido': {
-          templateUrl: 'templates/digiturno.html'
-        }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
       }
+    });
   })
-  .state('menu.detalles', {
-      url: '/detalles/:chatId',
-      views: {
-        'contenido': {
-          templateUrl: 'templates/detalles.html',
-          controller: 'DetallesCtrl'
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('menu', {
+        url: '/menu',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'MenuCtrl'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+      })
+      .state('menu.citas', {
+        url: '/citas',
+        views: {
+          'contenido': {
+            templateUrl: 'templates/citas.html'
+          }
+        }
+      })
+      .state('menu.digiturno', {
+        url: '/digiturno',
+        views: {
+          'contenido': {
+            templateUrl: 'templates/digiturno.html'
+          }
+        }
+      })
+      .state('menu.detalles', {
+        url: '/detalles/:chatId',
+        views: {
+          'contenido': {
+            templateUrl: 'templates/detalles.html',
+            controller: 'DetallesCtrl'
+          }
+        }
+      });
+
+    $urlRouterProvider.otherwise('/login');
+  })
+
+
+  .run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
+    $rootScope.$on('$stateChangeStart', function (event, next, nextParams, fromState) {
+      if (!AuthService.isAuthenticated()) {
+        console.log(next.name);
+        if (next.name !== 'login' && next.name !== 'outside.register') {
+          event.preventDefault();
+          $state.go('login');
         }
       }
     });
- 
-  $urlRouterProvider.otherwise('/login');
-})
- 
-
-
-.run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
-  $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
-    if (!AuthService.isAuthenticated()) {
-      console.log(next.name);
-      if (next.name !== 'login' && next.name !== 'outside.register') {
-        event.preventDefault();
-        $state.go('login');
-      }
-    }
   });
-});
--- www/js/services.js
+++ www/js/services.js
@@ -1,121 +1,124 @@
 angular.module('starter.services', [])
 
 
-//angular.module('starter')
- 
-.service('AuthService', function($q, $http, API_ENDPOINT) {
-  var LOCAL_TOKEN_KEY = 'yourTokenKey';
- // var username = '';
-  var isAuthenticated = false;
- // var role = '';
-  var authToken;
- 
-  function loadUserCredentials() {
-    var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
-    if (token) {
+  //angular.module('starter')
+
+  .service('AuthService', function ($q, $http, API_ENDPOINT) {
+    var LOCAL_TOKEN_KEY = 'yourTokenKey';
+    // var username = '';
+    var isAuthenticated = false;
+    // var role = '';
+    var authToken;
+
+    function loadUserCredentials() {
+      var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
+      if (token) {
+        useCredentials(token);
+      }
+    }
+
+    function storeUserCredentials(token) {
+      window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
       useCredentials(token);
     }
-  }
- 
-  function storeUserCredentials(token) {
-    window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
-    useCredentials(token);
-  }
- 
-  function useCredentials(token) {
+
+    function useCredentials(token) {
       isAuthenticated = true;
-    authToken = token;
- 
-    // Set the token as header for your requests!
-    $http.defaults.headers.common.Authorization = authToken;
-   /* username = token.split('.')[0];
-    isAuthenticated = true;
-    authToken = token;
- 
-    if (username == 'admin') {
-      role = USER_ROLES.admin
+      authToken = token;
+
+      // Set the token as header for your requests!
+      $http.defaults.headers.common.Authorization = authToken;
+      /* username = token.split('.')[0];
+       isAuthenticated = true;
+       authToken = token;
+
+       if (username == 'admin') {
+       role = USER_ROLES.admin
+       }
+       if (username == 'user') {
+       role = USER_ROLES.public
+       }
+
+       // Set the token as header for your requests!
+       $http.defaults.headers.common['X-Auth-Token'] = token;*/
     }
-    if (username == 'user') {
-      role = USER_ROLES.public
+
+    function destroyUserCredentials() {
+      authToken = undefined;
+      //username = '';
+      isAuthenticated = false;
+      $http.defaults.headers.common.Authorization = undefined;
+      //$http.defaults.headers.common['X-Auth-Token'] = undefined;
+      window.localStorage.removeItem(LOCAL_TOKEN_KEY);
     }
- 
-    // Set the token as header for your requests!
-    $http.defaults.headers.common['X-Auth-Token'] = token;*/
-  }
- 
-  function destroyUserCredentials() {
-    authToken = undefined;
-    //username = '';
-    isAuthenticated = false;
-    $http.defaults.headers.common.Authorization = undefined;
-    //$http.defaults.headers.common['X-Auth-Token'] = undefined;
-    window.localStorage.removeItem(LOCAL_TOKEN_KEY);
-  }
- //var login = function(name, pw) {
-  var login = function(user) {
-    return $q(function(resolve, reject) {
-      console.log('user ' + user);
-     // alert('loging buton');
-      $http.post(API_ENDPOINT.url + '/post', user).then(function(result) {
-       //  alert('result.data.args ' + JSON.stringify(result.data.json.name));
-        if (result.data.json.name == "admin") {
-          storeUserCredentials(result.data.token);
-          resolve(result.data.msg);
-        } else {
-          reject(result.data.msg);
-        }
+
+    //var login = function(name, pw) {
+    var login = function (user) {
+      return $q(function (resolve, reject) {
+        console.log('user ' + user);
+        // alert('loging buton');
+        $http.post(API_ENDPOINT.url + '/post', user).then(function (result) {
+          //  alert('result.data.args ' + JSON.stringify(result.data.json.name));
+          if (result.data.json.name == "admin") {
+            storeUserCredentials(result.data.token);
+            resolve(result.data.msg);
+          } else {
+            reject(result.data.msg);
+          }
+        });
       });
-    });
-   /* return $q(function(resolve, reject) {
-      if ((name == 'admin' && pw == '1') || (name == 'user' && pw == '1')) {
-        // Make a request and receive your auth token from your server
-        storeUserCredentials(name + '.yourServerToken');
-        resolve('Login success.');
-      } else {
-        reject('Login Failed.');
+      /* return $q(function(resolve, reject) {
+       if ((name == 'admin' && pw == '1') || (name == 'user' && pw == '1')) {
+       // Make a request and receive your auth token from your server
+       storeUserCredentials(name + '.yourServerToken');
+       resolve('Login success.');
+       } else {
+       reject('Login Failed.');
+       }
+       });*/
+    }
+
+    var logout = function () {
+      destroyUserCredentials();
+    };
+
+    var isAuthorized = function (authorizedRoles) {
+      if (!angular.isArray(authorizedRoles)) {
+        authorizedRoles = [authorizedRoles];
       }
-    });*/
-  }
- 
-  var logout = function() {
-    destroyUserCredentials();
-  };
- 
-  var isAuthorized = function(authorizedRoles) {
-    if (!angular.isArray(authorizedRoles)) {
-      authorizedRoles = [authorizedRoles];
-    }
-    return (isAuthenticated && authorizedRoles.indexOf(role) !== -1);
-  };
- 
-  loadUserCredentials();
- 
-  return {
-    login: login,
-    logout: logout,
-    isAuthorized: isAuthorized,
-    isAuthenticated: function() {return isAuthenticated;},
-   // username: function() {return username;},
-   // role: function() {return role;}
-  };
-})
+      return (isAuthenticated && authorizedRoles.indexOf(role) !== -1);
+    };
+
+    loadUserCredentials();
+
+    return {
+      login: login,
+      logout: logout,
+      isAuthorized: isAuthorized,
+      isAuthenticated: function () {
+        return isAuthenticated;
+      },
+      // username: function() {return username;},
+      // role: function() {return role;}
+    };
+  })
 
 
-.factory('AuthInterceptor', function ($rootScope, $q, AUTH_EVENTS) {
-  return {
-    responseError: function (response) {
-      $rootScope.$broadcast({
-        401: AUTH_EVENTS.notAuthenticated,
-        403: AUTH_EVENTS.notAuthorized
-      }[response.status], response);
-      return $q.reject(response);
-    }
-  };
-})
- 
-.config(function ($httpProvider) {
-  $httpProvider.interceptors.push('AuthInterceptor');
-})
+  .factory('AuthInterceptor', function ($rootScope, $q, AUTH_EVENTS) {
+    return {
+      responseError: function (response) {
+        $rootScope.$broadcast({
+          401: AUTH_EVENTS.notAuthenticated,
+          403: AUTH_EVENTS.notAuthorized
+        }[response.status], response);
+        return $q.reject(response);
+      }
+    };
+  })
+
+  .config(function ($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+  })
 
 
 
@@ -132,7 +135,7 @@ angular.module('starter.services', [])
 
 
 
-/**/.factory('Chats', function() {
+  /**/.factory('Chats', function () {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
@@ -170,14 +173,14 @@ angular.module('starter.services', [])
     face: 'img/angela.png',
     sexo: 'Femenino'
 
-  },  {
+  }, {
     id: 5,
     name: 'Roosvel Guinald',
     identificacion: '89009950',
     face: 'img/roosvel.png',
     sexo: 'Masculino'
 
-  },  {
+  }, {
     id: 6,
     name: 'Claudia Salazar',
     identificacion: '41936493',
@@ -187,13 +190,13 @@ angular.module('starter.services', [])
   }];
 
   return {
-    all: function() {
+    all: function () {
       return chats;
     },
-    remove: function(chat) {
+    remove: function (chat) {
       chats.splice(chats.indexOf(chat), 1);
     },
-    get: function(chatId) {
+    get: function (chatId) {
       for (var i = 0; i < chats.length; i++) {
         if (chats[i].id === parseInt(chatId)) {
           return chats[i];
